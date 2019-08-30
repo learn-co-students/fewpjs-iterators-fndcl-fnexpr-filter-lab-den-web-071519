1c423f79436b3877e0a9bbd1002179d291d2bacf
SwanHub <jdprince555@gmail.com>
2019-08-29 21:03:30 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const drivers = ["Bill", "Ted", "Bill"]
+
+function findMatching(array, string){
+  return array.filter(function(name){
+    return name.toLowerCase() == string.toLowerCase();
+  })
+}
+
+function fuzzyMatch(array, smallString){
+  return array.filter(function(name){
+    return name.slice(0, smallString.length) === smallString
+  })
+}
+
+function matchName(arrayOfObjects, string){
+  return arrayOfObjects.filter(function(singleObject){
+    return singleObject.name === string
+  })
+}
